fb982a3cbd3f0431b41cf67af66ed3b91310dfcd
feeh <feeh@DESKTOP-2FBFR4I>
2025-03-31 20:50:16 -0300

    ex 21 for while JAVA

---
+++ src/main/java/ws/unisul21/Unisul21.java
@@ -0,0 +1,13 @@
+
+package ws.unisul21;
+import javax.swing.JOptionPane;
+public class Unisul21 {
+
+    public static void main(String[] args) {
+        for (int i = 34; i < 57; i++) {
+            if (i % 2 == 0){
+            JOptionPane.showMessageDialog(null, i);
+            }
+        }
+    }
+}
